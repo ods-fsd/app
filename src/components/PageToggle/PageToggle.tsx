ede6300004d0eb3e4f5eb60418172796f9c70175
Stanislav Galasyk <126322660+StanislavHalasyk@users.noreply.github.com>
2026-02-28 20:27:17 +0200

    Stanislav (#32)
    * my profile
    
    * fixed style mistake and button
    
    * fixed color and buttom
    
    * fixed mistake in block TravellersStories

--- src/components/PageToggle/PageToggle.tsx
+++ src/components/PageToggle/PageToggle.tsx
@@ -2,27 +2,31 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styles from "./PageToggle.module.css";
+import css from "./PageToggle.module.css";
 
-const PageToggle = () => {
+export const PageToggle = () => {
   const pathname = usePathname();
 
-  const isOwnActive = pathname === "/profile/own";
+  // Визначаємо активний таб на основі поточного шляху
   const isSavedActive = pathname === "/profile/saved";
+  const isOwnActive = pathname === "/profile/own";
 
   return (
-    <div className={styles.toggleWrapper}>
-      <Link
-        href="/profile/own"
-        className={`${styles.tab} ${isOwnActive ? styles.active : ""}`}
-      >
-        Мої історії
-      </Link>
+    <div className={css.toggleWrapper}>
+      {/* ПЕРША ВКЛАДКА - Збережені історії */}
       <Link
         href="/profile/saved"
-        className={`${styles.tab} ${isSavedActive ? styles.active : ""}`}
+        className={`${css.tab} ${isSavedActive ? css.active : ""}`}
       >
-        Збережені
+        Збережені історії
+      </Link>
+
+      {/* ДРУГА ВКЛАДКА - Мої історії */}
+      <Link
+        href="/profile/own"
+        className={`${css.tab} ${isOwnActive ? css.active : ""}`}
+      >
+        Мої історії
       </Link>
     </div>
   );
